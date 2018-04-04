2f3916fb1d3f0be4d9c95770067ab07f54fb2ae7
Alexandre Silva <contato@silvalexandre.com>
2018-04-04 19:43:38 -0300

    Add blank & error layout

---
+++ layouts/blank.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-app
+    v-content
+      v-container(grid-list-xl)
+        v-layout(row wrap)
+          v-flex(xs12 sm8 offset-sm2 md6 offset-md3)
+            nuxt
+    snackbar
+</template>
+
+<script>
+import Snackbar from '~/components/Snackbar'
+
+export default {
+  components: { Snackbar }
+}
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .text-xs-center
+    .display-1.my-3 {{ error.statusCode }}
+    .subheading.mb-2(v-if="error.statusCode !== 404")
+      | {{ error.message.message ? error.message.message : error.message }}
+    .subheading.mb-2(v-else) Página não encontrada
+    v-btn(
+      color="primary"
+      :to="{name: 'index'}"
+      outline dark
+    ) Página inicial
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'blank'
+}
+</script>
