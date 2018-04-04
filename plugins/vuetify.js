6230e4a02f477a85386177d305ca7158936a3cc8
Alexandre Silva <contato@silvalexandre.com>
2018-04-04 19:43:24 -0300

    Change theme; Add shadow css

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,5 +1,11 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.deepPurple.darken3,
+    accent: colors.teal.accent3
+  }
+})
